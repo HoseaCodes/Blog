c7537f66422cee4867bb8ed6ce60054ddb7f2568
hosead6168 <dominique11h@yahoo.com>
2020-10-12 12:16:23 -0500

    mobile update

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -7,8 +7,8 @@ const ContactForm = () => {
             <p>Have a question? I am available for hire and open to any ideas of cooperation.</p>
             <form action="https://getform.io/f/7efda21f-ca67-48f6-8a1e-723776d4ae3b" method='POST'>
 
-                <input className='input' type="text" name="name" placeholder="Name" />
-                <input className='input' type="email" name="email" placeholder="Enter Email" />
+                <input className='contact-input' type="text" name="name" placeholder="Name" />
+                <input className='contact-input' type="email" name="email" placeholder="Enter Email" />
                 <textarea className='area' name="message" placeholder='Your Message' />
                 <input className='btn' type="submit" value="Submit" />
             </form>

51b6db5c5bbf32305cf11c5bf9d7eaf9979ab006
hosead6168 <dominique11h@yahoo.com>
2020-10-16 00:45:36 -0500

    Tablet

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -26,7 +26,7 @@ function Carousel() {
                     <p>Solo Project: Calorie Kitchen API - JavaScript, CSS, HTML - Created a single page application with client side rending with API and AJAX capabilities. The API accessed Spoonacular DB JSON data.</p>
                     <div style={{ display: 'flex', marginTop: '30px' }}>
                         <a className='link' href="https://hoseacodes.github.io/Calorie-Kitchen/" target="_blank" rel="noopener noreferrer">View Live Site</a>
-                        <a className='link' href="https://github.com/HoseaCodes/Calorie-Kitchen" target="_blank" rel="noopener noreferrer">View Github</a>
+                        <a className='link github' href="https://github.com/HoseaCodes/Calorie-Kitchen" target="_blank" rel="noopener noreferrer">View Github</a>
                     </div>
                 </div>
             </div>
@@ -57,7 +57,7 @@ function Carousel() {
                     <p>Solo Project: Career Connect - JavaScript, CSS, HTML, MongoDB, oAuth Google, Node.js, RESTful routing -  Created a web server using a templating engine and MVC model. Building all of the application from beginning to end with an ability to consume a third party API and convert its JSON components.</p>
                     <div style={{ display: 'flex', marginTop: '30px' }}>
                         <a className='link' href="https://careerconnect.herokuapp.com/" target="_blank" rel="noopener noreferrer">View Live Site</a>
-                        <a className='link' href="https://github.com/HoseaCodes/CareerConnect" target="_blank" rel="noopener noreferrer">View Github</a>
+                        <a className='link github' href="https://github.com/HoseaCodes/CareerConnect" target="_blank" rel="noopener noreferrer">View Github</a>
                     </div>
                 </div>
             </div>
@@ -74,7 +74,7 @@ function Carousel() {
                     <p className='p-extended'>Collaborative Project: Merge-Immersive — This application was developed with a full stack MERN M - MongoDB to store data, E - Express, a back-end framework, R - React, a client side framework, N - NodeJS (RESTful routing) - to run back end service and written in JavaScript including JWT authentication . Styling with Bootstrap and CSS. Problem: Students in the bootcamp have a difficult time connecting with current and past students including students from different disciplines. Another challenge is maintaining the ability to communicate and collaborate on projects. Solution:  Our application gives GA students the ability to see their current projects, websites and applications. Also, giving them the ability to reach out to current and past students even students that are not in their program type like Software Engineering and Data Science. Allowing students to connect and build a stronger network.</p>
                     <div style={{ display: 'flex', marginTop: '30px' }}>
                         <a className='link' href="http://www.mergeimmersive.com/" target="_blank" rel="noopener noreferrer">View Live Site</a>
-                        <a className='link' href="https://github.com/HoseaCodes/merge-immersive" target="_blank" rel="noopener noreferrer">View Github</a>
+                        <a className='link github' href="https://github.com/HoseaCodes/merge-immersive" target="_blank" rel="noopener noreferrer">View Github</a>
                     </div>
                 </div>
             </div>
@@ -90,7 +90,7 @@ function Carousel() {
                     <p className='p-extended'>Collabrative Project: DevCenter - Styling with Bootstrap, Materialize and CSS. Problem: Devs have multiple tabs windows open while attempting to do there job. Solution: To create an app that would allow the users to authenticate with other applications and then be able to use some base functionality of those sites via a third party API. </p>
                     <div style={{ display: 'flex', marginTop: '30px' }}>
                         <a className='link' href="https://devcenter-629.herokuapp.com/" target="_blank" rel="noopener noreferrer">View Live Site</a>
-                        <a className='link' href="https://github.com/HoseaCodes/DevCenter" target="_blank" rel="noopener noreferrer">View Github</a>
+                        <a className='link github' href="https://github.com/HoseaCodes/DevCenter" target="_blank" rel="noopener noreferrer">View Github</a>
                     </div>
                 </div>
             </div>
--- src/Components/Twitter/Timeline.jsx
+++ src/Components/Twitter/Timeline.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Timeline.css'
 import { Timeline } from 'react-twitter-widgets';
 
 const TweeterTimeline = () => {
     return (
-        <div>
+        <div className='twitter'>
             <Timeline dataSource={{
                 sourceType: "profile", screenName: "DominiqueRHosea",
             }}

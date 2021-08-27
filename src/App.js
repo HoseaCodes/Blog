866c0c2ed7ae6680e403911eb8c2be8e4f97d0db
hosead6168 <dominique11h@yahoo.com>
2021-08-27 12:43:55 -0500

    Removed big o post

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // import CareerConnect from './Pages/Projects/CareerConnect';
 // import MergeImmersive from './Pages/Projects/MergeImmersive';
 // import DevCenter from './Pages/Projects/DevCenter';
-import BigO from './Pages/Articles/Big0';
 import ReactSecurity from './Pages/Articles/ReactSecurity';
 import CreateArticle from './Pages/Articles/CreateArticle'
 import { DataProvider } from './GlobalState';
@@ -75,14 +74,6 @@ export default class App extends Component {
               />
             )}
           /> */}
-          <Route
-            exact
-            path="/bigo"
-            render={() => (
-              <BigO
-              />
-            )}
-          />
           <Route
             exact
             path="/reactsecurity"

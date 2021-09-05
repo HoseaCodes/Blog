d4f260ae21c1df24cc5d88d1788ca59b3fb80a18
hosead6168 <dominique11h@yahoo.com>
2021-09-05 17:33:44 -0500

    refactor(upload backend): added routes and controllers

--- routes/upload.js
+++ routes/upload.js
@@ -1,71 +1,19 @@
-import fs from 'fs';
-import dotenv from 'dotenv';
 import express from 'express';
-import cloudinary from 'cloudinary';
-
-dotenv.config();
+import {
+  uploadImage,
+  destoryImage,
+  getAllUploads
+} from '../controllers/upload.js';
 
 const router = express.Router();
 
-cloudinary.config({
-    cloud_name: process.env.CLOUND_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-})
-
+//image upload
+router.post("/allImages", getAllUploads);
 
 //image upload
-router.post("/upload", (req, res) => {
-    try {
-        if (!req.files || Object.keys(req.files).length === 0) return res.status(400).send({ msg: "No files were uploaded." })
-
-        const file = req.files.file;
-        if (file.size > 1024 * 1024) {
-            removeTmp(file.tempFilePath)
-            return res.status(400).json({ msg: "File size too large" })
-        }
-
-        if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
-            removeTmp(file.tempFilePath)
-            return res.status(400).json({ msg: "File format is incorrect" })
-        }
-
-        cloudinary.v2.uploader.upload(file.tempFilePath, { folder: "HoseaCodes" }, async (err, result) => {
-            if (err) throw err;
-
-            removeTmp(file.tempFilePath)
-
-            res.json({ result })
-        })
-
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json({ msg: err.message })
-    }
-})
+router.post("/upload", uploadImage);
 
 //image delete
-router.post("/destory", (req, res) => {
-    try {
-        const { public_id } = req.body
-        if (!public_id) return res.status(400).json({ msg: 'No images Selected' })
-
-        cloudinary.v2.uploader.destroy(public_id, async (err, result) => {
-            if (err) throw err;
-
-            res.json({ msg: 'Deleted Image' })
-        })
-
-    } catch (err) {
-        return res.status(500).json({ msg: err.message })
-    }
-
-})
-
-const removeTmp = (path) => {
-    fs.unlink(path, err => {
-        if (err) throw err;
-    })
-}
+router.post("/destory", destoryImage);
 
 export default router;

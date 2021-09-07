130f4fccf5e1ba88fcf3c8e3e39c7afc5c0371ec
hosead6168 <dominique11h@yahoo.com>
2021-09-06 22:05:35 -0500

    refactor(error): error handling page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 
 const NotFound = () => {
+  const location = useLocation();
     return (
         <React.Fragment>
-            <h1>
-                404! We couldn't find that!</h1>
+            <h1>404! We couldn't find that!</h1>
+            <h2>Resources not found ar {location.pathname}!</h2>
             <Link to='/'>Go Back Home</Link>
         </React.Fragment>
     )

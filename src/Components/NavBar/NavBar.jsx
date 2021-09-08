dab73bd9e21462b991b405ebf8d2d571e250dd7b
hosead6168 <dominique11h@yahoo.com>
2021-09-08 09:36:14 -0500

    refactor(nav): adj navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,45 +1,73 @@
-import React, {useReducer } from "react";
+import React, {useReducer, useContext } from "react";
 import "./NavBar.css";
-import burger from '../../icons/burger.jpg';
+import burger from '../../Assets/Images/burger-min.png';
 import { Link } from "react-router-dom";
-import Logo from '../../icons/logo.png';
+import Logo from '../../Assets/Images/logo-min.png';
+import { GlobalState } from '../../GlobalState';
+import {StyledHr} from '../../Layout/Hr/styledHr';
 
 
 const NavBar = () => {
-    const [isActive, toggle] = useReducer(
-        (isActive) => !isActive,
-        false
-        );
+  const state = useContext(GlobalState);
+  const [isLoggedIn] = state.userAPI.isLoggedIn
+  const [isAdmin] = state.userAPI.isAdmin
+  const [user] = state.userAPI.user
+  const [isActive, toggle] = useReducer(
+      (isActive) => !isActive,
+      false
+      );
+
     return (
-        <header className="header-nav">
+        <header className="header-nav conatiner">
             <div className='burger-nav'>
-                <img className='nav-img' src={burger} alt="menu" 
-                    width='50px' height='50px'
-                    onClick={toggle}
+                <img className='nav-img' src={burger} alt="menu"
+                width='50px' height='50px'
+                onClick={toggle}
                 />
             </div>
             <nav className='nav-combo'>
-                <img className='nav-logo' src={Logo} alt="HoseaCodes" />
+                {
+                  !isLoggedIn ?
+                  <img className='nav-logo' src={Logo} alt="HoseaCodes" />
+                  :
+                  <h1>Welcome, {user.name.split(' ')[0]}</h1>
+                }
                 <ul className={`left-nav ${isActive ? "" : "left-nav open"}`}>
                     <Link to="/" className="nav-link active">Home</Link>
                     <li className="dropdown">
-                        <a className="nav-link dropdown-toggle" href="/" 
-                        id="navdrop" role="button" data-toggle="dropdown" 
+                        <a className="nav-link dropdown-toggle" href="/"
+                        id="navdrop" role="button" data-toggle="dropdown"
                         data-hover="dropdown">Portfolio</a>
                         <div className="dropdown-menu" aria-labelledby="navdrop">
-                            <a href="http://www.dominiquehosea.com" rel="noopener noreferrer" 
+                            <a href="http://www.dominiquehosea.com" rel="noopener noreferrer"
                             target="_blank" className="dropdown-item nav-link">Backend Portfolio</a>
                             {/* <a href="/client" rel="noopener noreferrer" className="dropdown-item nav-link">Client Portfolio</a> */}
-                            <a href="/project" rel="noopener noreferrer" 
+                            <a href="/project" rel="noopener noreferrer"
                             className="dropdown-item nav-link">Project Case Studies</a>
                         </div>
                     </li>
                     <Link to="/blog" className="nav-link">Blog</Link>
                     <Link to="/about" className="nav-link">About</Link>
                     <Link to="/contact" className="nav-link">Contact</Link>
+                    {
+                      isLoggedIn ?
+                      <>
+                      <Link to="/profile" className="nav-link">Profile</Link>
+                      </>
+                      :
+                      null
+                    }
+                    {
+                      isAdmin ?
+                      <>
+                      <Link to="/settings" className="nav-link">Settings</Link>
+                      </>
+                      :
+                      null
+                    }
                 </ul>
             </nav>
-            <hr className="header-hr" />
+            <StyledHr Primary/>
         </header>
 
     )

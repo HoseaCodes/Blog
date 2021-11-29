9c0aab54dda2dabf72b90e897320248acfec73ab
hosead6168 <dominique11h@yahoo.com>
2021-11-29 09:57:43 -0600

    fix(contact): refactored contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,40 +1,23 @@
-//Make it easy for your audience to
-//contact you. You can create a separate
-//“Contact” section, a “Contact”
-//call-to-action button, or both. One
-//best practice is to include multiple,
-//easily accessible ways for your audience
-//to contact you, as they navigate their
-//way through your website. Include a
-//contact form, your email address, or
-//both in this section. It’s not necessary
-//to include your phone number or address
-//in your website’s contact information.
-
 import React from 'react';
-import './Contact.css'
-import NavBar from '../../Components/NavBar/NavBar';
 import ContactForm from '../../Components/Form/ContactForm'
 import { StyledHr } from '../../Layout/Hr/styledHr';
-import Footer from '../../Components/Footer/Footer';
+import { HeroContainer, HeroPositioning} from '../../Layout/Hero/styledHero';
+import { StyledH2 } from '../../Layout/Headers/styledH2';
+import { StyledContainer } from '../../Layout/Container/styledContainer';
 
 const Contact = () => {
     return (
-        <div >
-            <NavBar />
-            <div className='cHeader'>
-                <div className='header-logo'>
-                    <h1 className='heading-primary'>
-                        <span className="header-username"></span>
-                    </h1>
-                </div>
-            </div>
-            <StyledHr Primary/>
-            <h2 className='contacth2'>Contact Me</h2>
-            <ContactForm />
-            <StyledHr Primary/>
-            <Footer/>
-        </div >
+      <>
+        <HeroContainer Contact>
+          <HeroPositioning/>
+        </HeroContainer>
+        <StyledHr Primary/>
+        <StyledContainer >
+          <StyledH2>Contact Me</StyledH2>
+          <ContactForm />
+        </StyledContainer >
+        <StyledHr Primary/>
+        </>
     )
 
 }

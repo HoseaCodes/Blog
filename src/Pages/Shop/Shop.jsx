fa1ad2b37141046977ae1182506c1b30a65687b4
hosead6168 <dominique11h@yahoo.com>
2022-03-20 03:55:47 -0500

    feat(shop): added shop page

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import Footer from '../../Components/Footer/Footer';
+import NavBar from '../../Components/NavBar/NavBar';
+// import NavBar from '../../Components/NavBar/NavBar';
+import Products from '../../Components/Product/Products';
+import Sidebar from './Header';
+import './Shop.css';
 
 const Shop = () => {
     return (
-        <div>
-            <iframe title='hoseacodes-store' src='https://teespring.com/stores/hoseacodes-2'>
-            </iframe>
-        </div>
+          <div>
+            {/* <iframe title='hoseacodes-store' src='https://teespring.com/stores/hoseacodes-2'>
+            </iframe> */}
+            <NavBar/>
+            <div className="wrapper">
+              {/* <NavBar/> */}
+              <div className="shop-container">
+                  <Sidebar className="sidebar" />
+                  <Products/>
+              </div>
+            </div>
+            <Footer/>
+          </div>
     )
 }
 
